8a6362ec7918465d9f32a2318207400e94b6901f
Teza01x <110857514+teza01x@users.noreply.github.com>
2023-09-12 12:18:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 import os
 
 
-telegram_token = '6491882032:AAFni_-MEGXANIfjEfwm0sMaoMIGU2_8tUc'
+telegram_token = ''
 
-api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImU4YWE3ZTU4LTdjNDEtNGVjYi1iYTA2LWU5NWEzMGU5ZGE3MSIsIm9yZ0lkIjoiMzQ4NzA0IiwidXNlcklkIjoiMzYyODM4IiwidHlwZUlkIjoiNTlhNTAyZGMtMWJiNi00MDlkLTg3YzktN2ZiYmQxNzgzNTA5IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2OTIyNzI5NDcsImV4cCI6NDg0ODAzMjk0N30.YdWogfGR93OFGOiBTCxYJe2MH2T1dZkaAGJwcAiMnjk'
+api_key = ''
 
 data_base = os.path.join(os.path.dirname(__file__), 'database.db')
 
-# stream_id = 'a97074a2-c7f6-47b2-8156-a12a95d14450'
-stream_id = '0aeacbf5-5614-419a-aaf0-5fc297eb2d57'
+stream_id = ''
 
-image_path = 'D:\\PythonProjects\\crypto_telegram_bot\\wallet_images\\'
+image_path = '...\\wallet_images\\'
 
 price_diff_up = 100
 price_diff_down = 90
